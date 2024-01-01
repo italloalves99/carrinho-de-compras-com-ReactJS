bd8dfbac00202351e2629ac11e3eeb62dc44c5a9
italloalves99 <134318969+italloalves99@users.noreply.github.com>
2024-01-01 16:04:47 -0300

    listado produtos

---
+++ carrinho/src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import './Products.css';
+import fetchProducts from '../../api/fetchProducts';
+import ProductCard from '../ProductCard/ProductCard';
+function Products() {
+
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+         fetchProducts('ps4').then((response) => {
+            setProducts(response);
+         });
+    }, []);
+
+    return ( 
+       <section className='products container'>
+        {
+            products.map((product) => <ProductCard key={product.id} data={product} />)
+        }
+
+       </section> 
+     );
+}
+
+export default Products;
