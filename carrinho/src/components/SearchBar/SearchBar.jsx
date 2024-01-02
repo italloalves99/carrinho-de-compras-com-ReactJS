def02715981664114732901966c081d6c7cb44cb
italloalves99 <134318969+italloalves99@users.noreply.github.com>
2024-01-01 21:55:23 -0300

    Provider

--- carrinho/src/components/SearchBar/SearchBar.jsx
+++ carrinho/src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react'
 import { CiSearch } from "react-icons/ci";
 import './SearchBar.css';
+import fetchProducts from '../../api/fetchProducts';
 
 function SearchBar() {
 
     const [searchValue, setSearchValue] = useState('');
 
+    const handleSearch = async (event) => {
+        event.preventDefault();
+        
+        const products = await fetchProducts (searchValue);
+        console.log(products);
+        setSearchValue('');
+    };
+
     return (  
-        <form className='search-bar'>
-            <input type="search" value={searchValue} placeholder="Buscar Produtos" className="search__input" onChange={(event) => setSearchValue(event.target.value)} required/> 
+        <form className='search-bar' onSubmit={handleSearch}>
+            <input type="search" value={searchValue} placeholder="Buscar Produtos" className="search__input" onChange={(target) => setSearchValue(target.value)} required/> 
                 {searchValue}
             <button type="submit" className="search__button"><CiSearch /></button>
         </form>
